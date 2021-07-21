62cca98a9a99c8efc0f524bc25d4856e5b8659c1
Armen Derikyan <armen.derikyan@gmail.com>
2021-07-21 16:30:48 +0400

    Showing posts from profiles the user Is following

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
 // Route::get('/p', function () {
 //     return view("posts/create");
 // });
+
+Route::get('/','PostsController@index');
 
 Route::post('follow/{user}', 'FollowsController@store');
 
